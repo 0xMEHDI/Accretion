438eb41d9fab9e84074ae73d5cfd76d6b52b66b6
AccretionCD <56089902+AccretionCD@users.noreply.github.com>
2019-11-18 02:06:07 +0000

    New Project --- Added Basic Level + Player and Camera Controller

---
+++ no-name/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    PlayerController player;
+
+    void Awake()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);   
+    }
+}
---
+++ no-name/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 50f;
+    [SerializeField] float maxSpeed = 3f;
+    [SerializeField] float jumpForce = 150f;
+    [SerializeField] float maxForce = 2f;
+
+    Rigidbody2D rigidBody;
+    bool isGrounded;
+
+    void Start()
+    {              
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        ProcessHorizontalMovement();
+        ProcessVerticalMovement();
+    }
+
+    private void ProcessHorizontalMovement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        rigidBody.AddForce(Vector2.right * horizontal * speed);
+
+        if (rigidBody.velocity.x > maxSpeed)
+        {
+            rigidBody.velocity = new Vector2(maxSpeed, rigidBody.velocity.y);
+            transform.localScale = new Vector3(0.5f, transform.localScale.y, transform.localScale.z);
+        }
+
+        if (rigidBody.velocity.x < -maxSpeed)
+        {
+            rigidBody.velocity = new Vector2(-maxSpeed, rigidBody.velocity.y);
+            transform.localScale = new Vector3(-0.5f, transform.localScale.y, transform.localScale.z);
+        }
+    }
+
+    private void ProcessVerticalMovement()
+    {
+        float jump = Input.GetAxisRaw("Jump");
+        if (isGrounded)
+        {
+            rigidBody.AddForce(Vector2.up * jump * jumpForce);
+            if (rigidBody.velocity.y > maxForce)
+            {
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, maxForce);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+}
