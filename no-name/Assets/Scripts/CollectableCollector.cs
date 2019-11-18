5d41e5246509b5a6ed77b3ac84a0b72ab580a4c5
AccretionCD <56089902+AccretionCD@users.noreply.github.com>
2019-11-18 09:23:03 +0000

    Improved Camera, Level and added Collectables

---
+++ no-name/Assets/Scripts/CollectableCollector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableCollector : MonoBehaviour
+{
+    int coins = 0;
+    int keys = 0;
+   
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Coin")
+        {
+            Destroy(collision.gameObject);
+            coins++;
+        }
+        else if (collision.tag == "Key")
+        {
+            Destroy(collision.gameObject);
+            keys++;
+        }
+    }
+
+    public int getCoins()
+    {
+        return coins;
+    }
+
+    public int getKeys()
+    {
+        return keys;
+    }
+}
