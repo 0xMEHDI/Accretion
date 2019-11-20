0bde264d41da5695f940e8dbae27f4e02d75edd4
AccretionCD <56089902+AccretionCD@users.noreply.github.com>
2019-11-20 04:16:56 +0000

    Added AI lockon deadzone and reworked level

--- no-name/Assets/Scripts/EnemyAI.cs
+++ no-name/Assets/Scripts/EnemyAI.cs
@@ -6,22 +6,56 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] float lockOnSpeed = 2f;
     [SerializeField] float lockOnDistance = 5f;
+    [SerializeField] float lockOnDeadZone = 1f;
 
     Transform player;
     Rigidbody2D rb2d;
+    Animator animator;
+
+    bool moving;
 
     void Start()
     {
         player = FindObjectOfType<PlayerController>().transform;
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (Mathf.Abs(player.position.x - rb2d.position.x) <= lockOnDistance && Mathf.Abs(player.position.y - rb2d.position.y) <= lockOnDistance)
+        animator.SetBool("moving", moving);
+    }
+
+    void FixedUpdate()
+    {
+        if (Mathf.Abs(player.position.x - rb2d.position.x) < lockOnDistance && Mathf.Abs(player.position.y - rb2d.position.y) < lockOnDistance)
         {
-            Vector2 target = new Vector2(player.position.x, rb2d.position.y);
-            rb2d.position = Vector2.MoveTowards(rb2d.position, target, lockOnSpeed * Time.deltaTime);
+            if (Mathf.Abs(player.position.x - rb2d.position.x) > lockOnDeadZone)
+            {
+                Vector2 target = new Vector2(player.position.x, rb2d.position.y);
+                rb2d.position = Vector2.MoveTowards(rb2d.position, target, lockOnSpeed * Time.fixedDeltaTime);
+
+                moving = true ? rb2d.position.x != player.position.x : moving = false;
+
+                if (rb2d.position.x - player.position.x > Mathf.Epsilon)
+                {
+                    rb2d.transform.localScale = new Vector2(Mathf.Abs(rb2d.transform.localScale.x), rb2d.transform.localScale.y);
+                }
+                else if (rb2d.position.x - player.position.x < -Mathf.Epsilon)
+                {
+                    rb2d.transform.localScale = new Vector2(-Mathf.Abs(rb2d.transform.localScale.x), rb2d.transform.localScale.y);
+                }
+            }
+
+            else
+            {
+                moving = false;
+            }
+        }
+
+        else 
+        {
+            moving = false;
         }
     }
 }
---
+++ no-name/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+    [SerializeField] Transform groundCheck;
+    //[SerializeField] Transform wallLeftCheck;
+    //[SerializeField] Transform wallRightCheck;
+
+    Rigidbody2D rb2d;
+
+    [SerializeField] bool movingLeft = true;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (movingLeft)
+        {
+
+        }
+        else 
+        {
+            movingLeft = false;
+        }
+    }
+}
